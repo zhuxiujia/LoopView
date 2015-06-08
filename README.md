966f0a5187cafa21acadaeb96be602d795a5de02
zhuxiujie <zhuxiujie@hejiajinrong.com>
2015-06-08 16:48:47 +0800

    LoopViewPager 新增介绍

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 #介绍
+1: LoopView<br />
+2: LoopViewPager（新增）<br />
 可以无限循环，自动旋转，停靠的3D旋转布局控件,无需编写代码，直接在布局中加入自己的布局即可。<br />
 仅使用三角函数,使用ValueAnimation,继承FrameLayout.兼容所有滑动组件
 ##优势
@@ -7,7 +9,6 @@
 *可直接在xml添加元素即可添加列数据。无需编写代码添加<br />
 *优良的兼容性，和尺寸控制<br />
 ##如何使用？
-
 `<com.cry.animation.LoopView`<br />
         `android:layout_width="fill_parent"`<br />
         `android:layout_height="fill_parent"`<br />
--- app/src/main/java/com/cry/animation/MainActivity.java
+++ app/src/main/java/com/cry/animation/MainActivity.java
@@ -2,6 +2,7 @@ package com.cry.animation;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,31 +19,30 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        //LoopViewPager 使用方法---------------------------------------------
         pager=(LoopViewPager)findViewById(R.id.loopViewPager);
         List<View> arr=new ArrayList<View>();
-
-        for (int i=0;i<1;i++){
-            TextView textView=new TextView(this);
-            textView.setText("第" + i + "个");
-            textView.setBackgroundResource(R.drawable.image_red);
-            //textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
-            textView.setGravity(TextView.TEXT_ALIGNMENT_CENTER);
-            arr.add(textView);
+        for (int i=0;i<3;i++){
+            View v= LayoutInflater.from(this).inflate(R.layout.item_pager,null);
+            TextView textView=(TextView)v.findViewById(R.id.textView);
+            textView.setText("index"+i);
+            arr.add(v);
         }
         pager.setList(arr);
         try {
             pager.setAutoChange(true);
         } catch (Exception e) {
-            Toast.makeText(this,""+e.toString(),1000).show();
-            e.printStackTrace();
+            Toast.makeText(this,""+e.toString(),Toast.LENGTH_LONG).show();
         }
 
+
+
+        //LoopView 使用方法---------------------------------------------
         loopView=(LoopView)findViewById(R.id.loopView);
         loopView.setAutoRotationTime(3*1000);
         loopView.setAutoRotation(true);
         loopView.setR(200);
-
-
-
     }
 }
